42c27627d3883eddf0fdc013dcfc21178b43217e
on0423 <112231254+OtsukaNaoki@users.noreply.github.com>
2024-03-09 17:58:59 +0900

    Initial Commit

---
+++ MSS/MSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSSApp.swift
+//  MSS
+//
+//  Created by 大塚直樹 on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct MSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
